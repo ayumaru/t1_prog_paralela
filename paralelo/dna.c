4ee40d4605a32f4c82d8e2500cb3eebd20509b5e
maru <marllonwes@hotmail.com>
2021-06-05 20:46:22 -0300

    blah

--- paralelo/dna.c
+++ paralelo/dna.c
@@ -12,16 +12,14 @@ int bmhs(char *string, int n, char *substr, int m) {
 
 	int d[MAX];
 	int i, j, k;
-
-	// pre-processing
-	#pragma omp simd
+	// pre-processing 
+	#pragma omp simd //tacerto
 	for (j = 0; j < MAX; j++) // talvez de pra usar o pragm do smid aqui (so no primeiro, o segundo da pq os elementos nao estao anihados)
 		d[j] = m + 1;
 	
 	#pragma omp simd
 	for (j = 0; j < m; j++)
 		d[(int) substr[j]] = m - j;
-
 	// searching
 	i = m - 1;
 	while (i < n) {
@@ -31,11 +29,13 @@ int bmhs(char *string, int n, char *substr, int m) {
 			j--;
 			k--;
 		}
-		if (j < 0)
+		if (j < 0)	
+		{
 			return k + 1;
+		}
 		i = i + d[(int) string[i + 1]];
 	}
-
+	// printf("");
 	return -1;
 }
 
@@ -85,15 +85,15 @@ static inline void remove_eol(char *line) {
 }
 
 typedef struct{
-	int cab, com; //cabecalho e comeco
 	char text_cab[100];
+	char genoma[1000001];
 } marcador;
 
 marcador marcadores[1000]; // 1000 marcadores inicialmente, acho que nao passa disso
-char *dna_completo;
+int cabs;
 
 
-static inline void preprocessamento(){
+void preprocessamento(){
 //pegar a primeira linha ( cabecalho inicial) fora do while, e marcar a pulada de linha dele com um &
 //depois trabalhar em cima desse simbolo
 // os marcadores [i][i+1] sao representantes das posicoes de real inicio/parada para cada thread
@@ -102,35 +102,30 @@ static inline void preprocessamento(){
 // se for assim com um campo so para texto, entao vai mudar os marcadores. ai vai mudar os controladores de id
 // ta errando so a onde pegar o paraneu
 	char linha[100];
-	int j,i;
+	int j; //i;
 	fseek(fdatabase, 0, SEEK_SET);
 	fgets(linha, 100, fdatabase);
 	remove_eol(linha);
-	marcadores[0].cab = 0; // primeira posicao de caracter disponibilizado, preciso saber tambem o tamanho do cabecalho
-	marcadores[0].com = marcadores[j].cab; // + strlen(linha)+1; // onde realmente comeca a base.. prox linha apos o cabecalho
+
 	strcpy( marcadores[0].text_cab, linha );
-	j = i = 0;
-	dna_completo[0] = 0;
+	j = 0; //i = 0;
+
 	while(!feof(fdatabase))
 	{
 		fgets(linha, 100, fdatabase);
 		remove_eol(linha);
 		do { 
-				strcat(dna_completo + i, linha);
+				strcat(marcadores[j].genoma, linha);
 				
 				if (fgets(linha, 100, fdatabase) == NULL)
 					break;
 				remove_eol(linha);
-				i += 80;
 		} while (linha[0] != '>');
+		cabs++;
 		j++;
-		marcadores[j].cab = i; // marcador de [>] 0..N e sua posicao no texto
-		marcadores[j].com = marcadores[j].cab; // + strlen(linha)+1;
 		strcpy( marcadores[j].text_cab, linha );
-		printf("valor de i: %d \n", i);
 	}
-	marcadores[j+1].cab = marcadores[j+1].com = -1; //token para dizer que ali acabou
-	// dna_completo[ strlen(dna_completo) +1 ] = '\0';
+	cabs-=1;
 }
 
 char *bases;
@@ -138,6 +133,7 @@ char *str;
 
 int main(char** argv) {
 	clock_t t = clock();
+	cabs =0;
 	int nt = 0;// (int) *argv[1]; // ver outra forma de usar isso 
 	bases = (char*) malloc(sizeof(char) * 1000001);
 	if (bases == NULL) {
@@ -149,30 +145,28 @@ int main(char** argv) {
 		perror("malloc str");
 		exit(EXIT_FAILURE);
 	}
-	dna_completo = (char*) malloc(sizeof(char) * 1000001);
-	if (dna_completo == NULL) {
-		perror("malloc str");
-		exit(EXIT_FAILURE);
-	}
-	
+
 	openfiles();
 
+	// printf("bacs antes do openfile: %d \n",cabs);
 	preprocessamento();
-	for (int m = 0; ( marcadores[m].cab != -1 || marcadores[m].com != -1 ); m++ )
-		printf("posicao e valor: [ patogenico: %s \n ] [ m: %d ] ~ [cab: %d] ~ [com %d] ~ [elemento da pos: %c  %c] \n", marcadores[m].text_cab, m, marcadores[m].cab, marcadores[m].com, dna_completo[ (int)marcadores[m].cab ], dna_completo[ (int) marcadores[m].com] );
-
-	printf("balbla: %.300s \n", dna_completo);
-
-	return 0;
+	// printf("bacs dep do openfile: %d \n",cabs);
+	// for (int m = 0; ( marcadores[m].cab != -1 || marcadores[m].com != -1 ); m++ )
+	// 	printf("posicao e valor: [ patogenico: %s \n ] [ m: %d ] ~ [cab: %d] ~ [com %d] ~ [elemento da pos: %c  %c] \n", marcadores[m].text_cab, m, marcadores[m].cab, marcadores[m].com, dna_completo[ (int)marcadores[m].cab ], dna_completo[ (int) marcadores[m].com] );
+	// for (int y = 0; y < cabs; y++)
+	// {	
+	// 	printf("seq: %s \n tenho tamanho: %ld \n %.30s \n", marcadores[y].text_cab, strlen( marcadores[y].genoma ) ,marcadores[y].genoma);
+	// }
+	// return 0;
 	char desc_dna[100], desc_query[100];
 	char line[100];
 	int i, found, result;
+	int y,h=0;
 	fgets(desc_query, 100, fquery); 
 	remove_eol(desc_query);
 	while (!feof(fquery)) {
 		fprintf(fout, "%s\n", desc_query);
 		// read query string
-		#pragma omp parallel num_threads(nt)
 		fgets(line, 100, fquery); // vai precisar de barreira 
 		remove_eol(line);
 		str[0] = 0;
@@ -192,32 +186,25 @@ int main(char** argv) {
 		fgets(line, 100, fdatabase);
 		remove_eol(line);
 		
-		
-		while (!feof(fdatabase)) { // focar aqui, talvez um pre processamento do dna ajude
-			
-			// ai faz um map se pa para criar vetores e tals 
-			strcpy(desc_dna, line);
-			bases[0] = 0;
-			i = 0;
-			fgets(line, 100, fdatabase);
-			remove_eol(line);
-			do { // mesmo esquema do la de cima, tentar fazer paralelo a consulta da base
-				strcat(bases + i, line);
-				if (fgets(line, 100, fdatabase) == NULL)
-					break;
-				remove_eol(line);
-				i += 80;
-			} while (line[0] != '>');
+		// printf("antes do for: %d \n", h);
+		for (y = 0; y <=cabs; y++ )
+		{	
+			// printf("entrei no for meu valor> %d ||| hzao: %d \n", y, h );
 
-			result = bmhs(bases, strlen(bases), str, strlen(str));
+			// printf("pos result \n");
+			result = bmhs( marcadores[y].genoma, strlen( marcadores[y].genoma ), str, strlen(str)); //str eh a query
+			// printf(" ** entrei pos for meu valor> %d ||| hzao: %d \n", y, h );
 			if (result > 0) {
-				fprintf(fout, "%s\n%d\n", desc_dna, result);
+				// printf("antes de escrever o result");
+				fprintf(fout, "%s\n%d\n", marcadores[y].text_cab, result);
 				found++;
+				// printf("depois de escrever o result");
 			}
+			// printf("pos if\n");
 		}
-
 		if (!found)
 			fprintf(fout, "NOT FOUND\n");
+		h++;
 	}
 
 	closefiles();
